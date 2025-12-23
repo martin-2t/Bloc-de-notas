65592676d9b3433f1fdc95a629009c2ea27f7fdb
martin-2t <martins495728@gmail.com>
2025-12-23 13:40:23 -0300

    Arreglado WSGI y settings para deploy en Render

--- mi_proyecto/mi_proyecto/settings.py
+++ mi_proyecto/mi_proyecto/settings.py
@@ -127,3 +127,5 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
+
+
--- mi_proyecto/mi_proyecto/wsgi.py
+++ mi_proyecto/mi_proyecto/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_proyecto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_proyecto.mi_proyecto.settings')
+
 
 application = get_wsgi_application()
