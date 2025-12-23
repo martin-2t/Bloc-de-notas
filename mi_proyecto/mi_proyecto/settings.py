859b0819579b745ab6fac63d1682042ac54a287a
martin-2t <martins495728@gmail.com>
2025-12-23 15:30:21 -0300

    últimos cambios

--- mi_proyecto/mi_proyecto/settings.py
+++ mi_proyecto/mi_proyecto/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mi_proyecto.mi_app',
+    'mi_app',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mi_proyecto.mi_proyecto.urls'
+ROOT_URLCONF = 'mi_proyecto.urls'
 
 
 TEMPLATES = [
