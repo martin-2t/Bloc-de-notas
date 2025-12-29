50743f5382267b6df79cc50bf4d2ec46aed534d9
martin-2t <martins495728@gmail.com>
2025-12-29 16:40:01 -0300

    Configurar static files con WhiteNoise

--- servidor_web/mi_proyecto/settings.py
+++ servidor_web/mi_proyecto/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,3 +135,4 @@ LOGOUT_REDIRECT_URL = '/login/'
 
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
